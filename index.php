4c143632dde439d3edb3ab69d75af52badc5736c
YAGO <yagocarlos11@outlook.com>
2025-02-10 20:26:22 -0300

    aula 10/02

--- index.php
+++ index.php
@@ -8,6 +8,47 @@
     <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
+
+<div class="interface">
+
+    <header class="cabecalho">
+
+         <nav class="menu">
+
+          <div class="logo">
+            <img src="imagens/logo.png" alt="LOGO" width="600" />
+          </div>
+
+           <ul>
+                 <li>Home</li>
+                 <li>Serviços</li>
+                 <li>Produtos</li>
+                 <li>Sobre</li>
+                 <li>Contato</li>
+                
+           </ul>
+
+           
+
+         </nav>
+
+         <div class="titles">
+
+           <h1 classe="titulo">Entregando serviços de qualidade</h1>
+           <h1 classe="subtitulo">Soluçoes em TI</h1>
+            <p class="pragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt delectus alias ipsa facere enim, accusamus voluptatum ex eius aut eveniet soluta corrupti perferendis explicabo fugit veritatis qui aliquam ipsam! Adipisci.</p>
+         </div>
+
+         <div class="socialicons">
+               <a href="#">Twitter</a>
+               <a href="#">Facebook</a>
+               <a href="#">LinkeDin</a>
+               <a href="#">Instagram</a>
+         </div>
+
+     </header>
+
+</div>
     
 </body>
 </html>
