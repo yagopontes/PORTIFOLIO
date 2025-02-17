3eceaa19cfd05b73037a33286597ef675ea1a226
YAGO <yagocarlos11@outlook.com>
2025-02-17 20:17:11 -0300

    aula 17/02

---
+++ Nova pasta/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PORTIFOLIO</title>
+
+    <link rel="stylesheet" href="css/styles.css" />
+    
+    <style>
+        {
+            margin: 0;
+            padding: 0;
+        }
+
+        body{
+            background-image: url('imagens/logo.png');
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 80%;
+            background-attachment: fixed;
+        }
+
+        p{
+          /*border:1px solid blue;*/
+            border-width: 1px; 
+            border-style: solid;
+            border-color: blue;
+        }
+
+        h1{
+            background: yellow;
+            display:block;
+        }
+        H1:HOVER{
+
+        }
+        .logo{
+            position:absolute;
+            top:0;
+            right
+        }
+
+    </style>
+</head>
+<body>
+
+<div class="interface">
+
+ <header class="cabecalho">
+
+  <nav class="menu">
+    <div class="logo">
+     <img src="imagens/logo.png" alt="LOGO" width="600" />
+    </div>
+    <ul>
+        <li>Home</li>
+        <li>Serviços</li>
+        <li>Produtos</li>
+        <li>Portifolio</li>
+        <li>Sobre</li>
+        <li>Contato</li>
+    </ul>
+
+
+
+    <ol>
+        
+    </ol>
+
+  </nav>
+
+  <div class="titles">
+      <h1 class="titulo">Entregando serviços de qualidade</h1>
+      <h1 class="subtitulo">Soluções em TI</h1>
+
+      <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deleniti ad veritatis voluptatibus officia vel.</p>
+  </div>
+
+  <div class="socialIcons">
+     <a href="#">Twitter</a>
+     <a href="#">Facebook</a>
+     <a href="#">linkednl</a>
+     <a href="#">Instagram</a>
+  </div>
+
+ </header>
+
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,73 +1,145 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PORTIFOLIO</title>
 
+
     <style>
-        *{
-          margin:  0;   
-          padding: 0;   
-        }
-        p {
-          border: 1px solid green;
-        }
 
-        body{
-                 background-image: url("imagens/logo.png")
-                 background-repeat: no-repeat;
-                 background-position: center;
-                 background-repeat: no-repeat;
-                }
+    .root {
+        font-size: 12px;
 
-        h1 {
-          background: yellow;
-        }
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: arial, helvetica, serif; 
+        
+    }
+
+    body {
+        background-image: url('imagens/logo.png');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 70%;
+        background-attachment: fixed; 
+
+        border: 10px solid blue;
+        width: 50;
+        height: 100;
+        overflow: hidden;
+    }
+
+    p {
+        /* border: 1px solid red; */
+        border-width: 8px;
+        border-style: solid;
+        border-color: blue;
+        font-size: 1rem;
+
+    }
+
+    h1 {
+        background: orange;
+        display: inline-block;
+    
+    }
+
+    h1:hover {
+        background-color: red;
+
+    }
+
+    .logo {
+
+    }
+
+
     </style>
+
+
 </head>
 <body>
+    <div class="interface">
 
-<div class="interface">
+        <header class="cabecalho">
+            <nav class="menu">
+                <div class="logo">
+                    <img src="imagens/logo.png" alt='LOGO' width='400'>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>Serviços</li>
+                    <li>Produtos</li>
+                    <li>Portfólio</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
 
-    <header class="cabecalho">
 
-         <nav class="menu">
+            <div class="titles">
+                <h1 class="title">Entregando Serviços de Qualidade</h1>
+                <h1 class="subtitle">Soluções em T.I</h1>
+                <p class='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus obcaecati, illo doloribus iusto sit numquam nam laboriosam?</p>
+            </div>
 
-          <div class="logo">
-            <img src="imagens/logo.png" alt="LOGO" width="600" />
-          </div>
+            <div class="socialIcons">
+                <a href="#" target="_blank">Twitter</a>
+                <a href="#">Facebook</a>
+                <a href="#">LinkedIn</a>
+                <a href="#">Instagram</a>
+            </div>
+        </header>
 
-           <ul>
-                 <li>Home</li>
-                 <li>Serviços</li>
-                 <li>Produtos</li>
-                 <li>Sobre</li>
-                 <li>Contato</li>
-                
-           </ul>
+    </div>
 
-           
-
-         </nav>
-
-         <div class="titles">
-
-           <h1 classe="titulo">Entregando serviços de qualidade</h1>
-           <h1 classe="subtitulo">Soluçoes em TI</h1>
-            <p class="pragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus deleniti corporis pariatur nostrum fugit! Aliquam, quidem modi autem recusandae natus, magnam aspernatur maiores ullam aut veritatis, est atque aliquid sit?</p>
-         </div>
-
-         <div class="socialicons">
-               <a href="#" target="_blank">Twitter</a>
-               <a href="#">Facebook</a>
-               <a href="#">LinkeDin</a>
-               <a href="#">Instagram</a>
-         </div>
-
-     </header>
-
-</div>
-    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 </body>
 </html>
