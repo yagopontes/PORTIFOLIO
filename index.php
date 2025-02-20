3c71f8b8fe7c60ace7f2a4e017ebf6d69ece0044
YAGO <yagocarlos11@outlook.com>
2025-02-20 20:23:14 -0300

    aula 20/02/25

--- index.php
+++ index.php
@@ -6,59 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PORTIFOLIO</title>
 
+    <link rel="stylesheet" href="css/styles.css" />
 
-    <style>
-
-    .root {
-        font-size: 12px;
-
-    }
-    * {
-        margin: 0;
-        padding: 0;
-        font-family: arial, helvetica, serif; 
-        
-    }
-
-    body {
-        background-image: url('imagens/logo.png');
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: 70%;
-        background-attachment: fixed; 
-
-        border: 10px solid blue;
-        width: 50;
-        height: 100;
-        overflow: hidden;
-    }
-
-    p {
-        /* border: 1px solid red; */
-        border-width: 8px;
-        border-style: solid;
-        border-color: blue;
-        font-size: 1rem;
-
-    }
-
-    h1 {
-        background: orange;
-        display: inline-block;
-    
-    }
-
-    h1:hover {
-        background-color: red;
-
-    }
-
-    .logo {
-
-    }
-
-
-    </style>
 
 
 </head>
@@ -66,8 +15,11 @@
     <div class="interface">
 
         <header class="cabecalho">
+
             <nav class="menu">
+
                 <div class="logo">
+
                     <img src="imagens/logo.png" alt='LOGO' width='400'>
                 </div>
                 <ul>
@@ -79,7 +31,7 @@
                     <li>Contato</li>
                 </ul>
             </nav>
-
+        
 
             <div class="titles">
                 <h1 class="title">Entregando Serviços de Qualidade</h1>
@@ -97,49 +49,6 @@
 
     </div>
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+   
 </body>
 </html>
