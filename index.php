4e7fd032f1c52f33dda6d29c0f236d0ba5fa5df9
YAGO <yagocarlos11@outlook.com>
2025-03-13 20:37:01 -0300

    aula 13/03/2025

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
                 <li><a href="#">Portifolio</a></li>
                 <li><a href="#">Sobre</a></li>
                 <li><a href="#">Contato</a></li>
+                <li><a href="#">Restrito</a></li>
                 
             </ul>
 
@@ -55,6 +56,63 @@
          </div>
 
     </header>
+
+    <!''=================================================================''>
+
+    <main id="corpo">
+
+        <section class="servicos">
+
+            <article class="servico">
+                <img src="imagens/service-design.svg" alt="Design">
+                <h3 class="title-servico">Desenvolvimento WEB</h3>
+                <p class="text-servico">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis omnis, voluptas error impedit consectetur vitae.</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-hosting.svg" alt="Hosting">
+                <h3 class="title-servico">WEB Hosting</h3>
+                <p class="text-servico">Possimus vel temporibus deleniti repudiandae in similique unde necessitatibus odit corporis nihil soluta, ad repellendus?</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-social.svg" alt="Social Media">
+                <h3 class="title-servico">Mídias Sociais</h3>
+                <p class="text-servico">Earum mollitia a, quaerat magni dignissimos, sint velit libero minus magnam saepe perferendis aspernatur? Libero!</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-seo.svg" alt="SEO">
+                <h3 class="title-servico">Aplicações SEO</h3>
+                <p class="text-servico">Maiores aliquid sequi expedita. Rerum itaque nihil maxime at explicabo similique recusandae saepe magnam a!</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-cloud.svg" alt="Cloud">
+                <h3 class="title-servico">Computação em Nuvem</h3>
+                <p class="text-servico">Provident velit fuga consequuntur quae, iste accusamus repellendus, officiis quibusdam, odio doloribus voluptates iusto perspiciatis.</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-secure.svg" alt="Security">
+                <h3 class="title-servico">Segurança de Dados</h3>
+                <p class="text-servico">Expedita illo fugit error in quidem qui, architecto eligendi quae, perspiciatis modi non ab aliquid.</p>
+            </article>
+
+        </section>
+
+        <section class="portifolio">
+
+
+
+        </section>
+
+        <section class="sobre">
+
+
+
+        </section>
+
+
+    </main>
+
+
+
 
 </div>
     
