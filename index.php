a6f12103c9b54807f9e932a088a9002af492c152
YAGO <yagocarlos11@outlook.com>
2025-03-20 20:21:23 -0300

    aula 20/03/25

--- index.php
+++ index.php
@@ -118,6 +118,22 @@
 
         </section>
 
+        <section class="banner">
+
+        <hgroup class="container-texto">
+        <h4 class="title-banner">Saiba mais sobre nossos Projetos</h4>
+
+<p class="txt-banner">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis deleniti mollitia eius tempore beatae hic labore et. Iste aliquid facilis, numquam fugiat fugit distinctio qui.</p>
+
+<button class="btn-banner">Saiba mais </button>
+
+
+
+        </hgroup>
+
+
+        </section>
+
         <section class="sobre">
 
 
