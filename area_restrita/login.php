50d119ffda216d5ee594c2bd59bd1bc2241f3c2a
Tauan <yagocarlos11@outlook.com>
2025-04-14 20:28:00 -0300

    aula 14/04/25

--- area_restrita/login.php
+++ area_restrita/login.php
@@ -5,20 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Área Restrita - Login</title>
 
+    <link rel="stylesheet" href="../tools/fontawesome/css/all.css" />
     <link rel="stylesheet" href="css/login.css" />
 </head>
 <body>
     <div id="interface">
         <form action="" method="POST">
-            <h3 class="title-login">Digite seu usuário e senha</h3>
+            <span class="login-user"><i class="fas fa-user"></i></span>
 
-            <input type="email" name="txt_email" id="txt_email" class="" placeholder="Digite seu e-mail" />
-            <input type="password" name="txt_pw" id="txt_pw" class="" placeholder="Digite sua senha" />
+            <input type="email" name="txt_email" id="txt_email" class="" placeholder="Digite seu e-mail" required />
+            <input type="password" name="txt_pw" id="txt_pw" class="" placeholder="Digite sua senha" required />
 
-            <div class="container-buttons">
-                <a href="../">Cancelar</a>
-                <button type="submit" name="bt-submit" id="bt-submit" class="">Entrar</button>
-            </div>
+           
+                <input type="submit" name="bt-submit" id="bt-submit" value="Entrar" />
+            
         </form>
     </div>
 </body>
